92efe749f9700334a1fbb1b42d0395c5d360b451
Naresh Medisetty <naresh@ti.com>
2009-04-03 06:55:39 -0400

    5/9 ARM: DaVinci: ASoC: Adds Audio Serial Port support for TI DM646X processor
    Adds Audio Serial Port support for TI DM646X processor
    
    Signed-off-by: Steve Chen <schen@mvista.com>
    Signed-off-by: Naresh Medisetty <naresh@ti.com>

--- arch/arm/mach-davinci/include/mach/asp.h
+++ arch/arm/mach-davinci/include/mach/asp.h
@@ -6,16 +6,26 @@
 
 #include <mach/irqs.h>
 
-/* Bases of register banks */
+/* Bases of dm644x and dm355 register banks */
 #define DAVINCI_ASP0_BASE	0x01E02000
 #define DAVINCI_ASP1_BASE	0x01E04000
 
-/* EDMA channels */
+
+/* Bases of dm646x register banks */
+#define	DAVINCI_DM646X_MCASP0_REG_BASE		0x01D01000
+#define DAVINCI_DM646X_MCASP1_REG_BASE		0x01D01800
+
+/* EDMA channels of dm644x and dm355 */
 #define DAVINCI_DMA_ASP0_TX	2
 #define DAVINCI_DMA_ASP0_RX	3
 #define DAVINCI_DMA_ASP1_TX	8
 #define DAVINCI_DMA_ASP1_RX	9
 
+
+/* EDMA channels of dm646x */
+#define	DAVINCI_DM646X_DMA_MCASP0_AXEVT0	6
+#define	DAVINCI_DM646X_DMA_MCASP0_AREVT0	9
+#define	DAVINCI_DM646X_DMA_MCASP1_AXEVT1	12
 /* Interrupts */
 #define DAVINCI_ASP0_RX_INT	IRQ_MBRINT
 #define DAVINCI_ASP0_TX_INT	IRQ_MBXINT
