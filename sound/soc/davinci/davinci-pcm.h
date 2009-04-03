0b6d57f87138b63d1045dd30e06cfd7f75f615ef
Naresh Medisetty <naresh@ti.com>
2009-04-03 06:49:47 -0400

    4/9 ARM: DaVinci: ASoC: Adds ALSA PCM interface for the TI DM646X processor
    Adds ALSA PCM interface for the TI DM646X processor
    
    Signed-off-by: Steve Chen <schen@mvista.com>
    Signed-off-by: Naresh Medisetty <naresh@ti.com>

--- sound/soc/davinci/davinci-pcm.h
+++ sound/soc/davinci/davinci-pcm.h
@@ -12,18 +12,32 @@
 #ifndef _DAVINCI_PCM_H
 #define _DAVINCI_PCM_H
 
+#include <mach/edma.h>
+
 struct davinci_pcm_dma_params {
-	char *name;		/* stream identifier */
-	int channel;		/* sync dma channel ID */
-	dma_addr_t dma_addr;	/* device physical address for DMA */
-	unsigned int data_type;	/* xfer data type */
 	u8 cc_inst;
+	char *name;			/* stream identifier */
+	int channel;			/* sync dma channel ID */
+	dma_addr_t dma_addr;		/* device physical address for DMA */
+	unsigned int data_type;		/* xfer data type */
+	enum dma_event_q eventq_no;	/* event queue number */
 };
 
 struct evm_snd_platform_data {
+	char *clk_name;
 	int tx_dma_ch;
 	int rx_dma_ch;
 	u8 cc_inst;
+	u32 tx_dma_offset;
+	u32 rx_dma_offset;
+	enum dma_event_q eventq_no;	/* event queue number */
+	unsigned int codec_fmt;
+
+	/* McASP specific fields */
+	int tdm_slots;
+	u8 op_mode;
+	u8 num_serializer;
+	u8 *serial_dir;
 };
 
 extern struct snd_soc_platform davinci_soc_platform;
