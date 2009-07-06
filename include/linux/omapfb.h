b28f2ec7763fd3435664b5c3ee67236e3a69e53e
Mayuresh Janorkar <mayur@ti.com>
2009-07-06 00:00:00 -0500

    DSS2: DISPC configuration management
    This commit implements caching and synchronization of DISPC shadow
    registers.
    
    Disabled fifomerging, enabling it needs more thought and
    work.
    
    Signed-off-by: Mayuresh Janorkar <mayur@ti.com>

--- include/linux/omapfb.h
+++ include/linux/omapfb.h
@@ -53,6 +53,8 @@
 #define OMAPFB_QUERY_MEM	OMAP_IOW(56, struct omapfb_mem_info)
 #define OMAPFB_WAITFORVSYNC	OMAP_IO(57)
 #define OMAPFB_MEMORY_READ	OMAP_IOR(58, struct omapfb_memory_read)
+#define OMAPFB_GET_OVERLAY_COLORMODE OMAP_IOR(59, struct omapfb_ovl_colormode)
+#define OMAPFB_WAITFORGO	OMAP_IO(60)
 
 #define OMAPFB_CAPS_GENERIC_MASK	0x00000fff
 #define OMAPFB_CAPS_LCDC_MASK		0x00fff000
