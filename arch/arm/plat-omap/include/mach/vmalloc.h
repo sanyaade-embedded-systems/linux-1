270f058a9b769421651a3c578d1ae6a6403a3de6
Santosh Shilimkar <santosh.shilimkar@ti.com>
2009-07-09 20:16:17 +0530

    From: Santosh Shilimkar <santosh.shilimkar@ti.com>
    OMAP4: Adjust the vmalloc end
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

--- arch/arm/plat-omap/include/mach/vmalloc.h
+++ arch/arm/plat-omap/include/mach/vmalloc.h
@@ -17,5 +17,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
+#ifdef CONFIG_ARCH_OMAP4
+#define VMALLOC_END	  (PAGE_OFFSET + 0x10000000)
+#else
 #define VMALLOC_END	  (PAGE_OFFSET + 0x18000000)
+#endif
 
