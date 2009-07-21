a4075d7f3fa904bfe7e4af7d19ac6ff9dcb35da8
Misael Lopez Cruz <x0052729@ti.com>
2009-07-21 13:20:22 -0500

    ASoC: SDP4430: Request irq for TWL6030 NAUDINT
    SDP4430 machine uses IRQ119 for TWL6030 audio interrupt
    (NAUDINT), workqueue and wait_for_completion are initialized
    as required for CODEC driver to handle the interrupt.
    
    Signed-off-by: Misael Lopez Cruz <x0052729@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -20,6 +20,7 @@
  */
 
 #include <linux/clk.h>
+#include <linux/irq.h>
 #include <linux/platform_device.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
@@ -175,6 +176,7 @@ static void twl6030_codec_enable(int enable)
 
 static struct twl6030_setup_data twl6030_setup = {
 	.codec_enable = twl6030_codec_enable,
+	.irq = INT_44XX_AUDINT_IRQ,
 };
 
 /* Audio machine driver */
@@ -220,12 +222,26 @@ static int __init sdp4430_soc_init(void)
 
 	gpio_direction_output(TWL6030_AUDPWRON_GPIO, 0);
 
-	ret = platform_device_add(sdp4430_snd_device);
+	/* audio interrupt */
+	INIT_WORK(&twl6030_setup.audint_work, twl6030_naudint_work);
+
+	init_completion(&(twl6030_setup.ready_completion));
+
+	ret = request_irq(twl6030_setup.irq,
+		twl6030_naudint_handler,
+		IRQF_TRIGGER_HIGH | IRQF_DISABLED,
+		"soc-audio",
+		&twl6030_setup);
 	if (ret)
 		goto err2;
 
-	return 0;
+	ret = platform_device_add(sdp4430_snd_device);
+	if (ret)
+		goto err3;
 
+	return 0;
+err3:
+	free_irq(twl6030_setup.irq, &twl6030_setup);
 err2:
 	gpio_free(TWL6030_AUDPWRON_GPIO);
 err1:
@@ -238,6 +254,7 @@ module_init(sdp4430_soc_init);
 
 static void __exit sdp4430_soc_exit(void)
 {
+	free_irq(twl6030_setup.irq, &twl6030_setup);
 	gpio_free(TWL6030_AUDPWRON_GPIO);
 	platform_device_unregister(sdp4430_snd_device);
 }
