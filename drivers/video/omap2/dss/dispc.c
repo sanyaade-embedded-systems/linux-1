53ffda3563253486bf546adc77c914e60de14d89
Mayuresh Janorkar <mayur@ti.com>
2009-07-28 00:00:00 -0500

    Fixing PM related problems for DSS2 display controller
    Signed-off-by: Mayuresh Janorkar <mayur@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -3144,12 +3144,6 @@ int dispc_init(void)
 			DSSERR("Failed to get dpll4_m4_ck\n");
 			return -ENODEV;
 		}
- 	} else if (cpu_is_omap44xx()) {
-               dispc.dpll4_m4_ck = clk_get(NULL, "dpll4_m4_ck");
-               if (IS_ERR(dispc.dpll4_m4_ck)) {
-                      	DSSERR("Failed to get dpll4_m4_ck\n");
-                       	return -ENODEV;
-               	}	
 	}
 
 
