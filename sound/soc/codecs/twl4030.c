212c5d2bb364fd7a3878ea57d95df947c842894c
Jorge Eduardo Candelaria <x0107209@ti.com>
2009-08-15 03:05:36 -0500

    ASOC: TWL4030: Add mixer control for APLL configuration
    ASOC: TWL4030: Add mixer control for APLL configuration
    
    Add a mixer control to let user space enable the Audio PLL
    module when needed.
    
    PLL is needed to generate the clocks used for multimedia and audio
    processing.
    
    Signed-off-by: Jorge Eduardo Candelaria <x0107209@ti.com>

--- sound/soc/codecs/twl4030.c
+++ sound/soc/codecs/twl4030.c
@@ -1005,6 +1005,15 @@ static DECLARE_TLV_DB_SCALE(digital_capture_tlv, 0, 100, 0);
  */
 static DECLARE_TLV_DB_SCALE(input_gain_tlv, 0, 600, 0);
 
+static const char *twl4030_apll_ctl_texts[] = {
+	"APLL disabled", "APLL enabled"
+};
+
+static const struct soc_enum twl4030_apll_ctl_enum =
+	SOC_ENUM_SINGLE(TWL4030_REG_APLL_CTL, 4,
+			ARRAY_SIZE(twl4030_apll_ctl_texts),
+			twl4030_apll_ctl_texts);
+
 static const char *twl4030_rampdelay_texts[] = {
 	"27/20/14 ms", "55/40/27 ms", "109/81/55 ms", "218/161/109 ms",
 	"437/323/218 ms", "874/645/437 ms", "1748/1291/874 ms",
@@ -1105,6 +1114,8 @@ static const struct snd_kcontrol_new twl4030_snd_controls[] = {
 
 	SOC_DOUBLE_TLV("Analog Capture Volume", TWL4030_REG_ANAMIC_GAIN,
 		0, 3, 5, 0, input_gain_tlv),
+
+	SOC_ENUM("PLL power control", twl4030_apll_ctl_enum),
 
 	SOC_ENUM("HS ramp delay", twl4030_rampdelay_enum),
 
