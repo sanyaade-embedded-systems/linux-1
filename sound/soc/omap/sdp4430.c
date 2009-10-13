651639173a5d0005ec84ebcfd4fdbbd52b237bab
Jorge Eduardo Candelaria <x0107209@ti.com>
2009-10-12 19:04:10 -0500

    ASoC: SDP4430: Add HDMI dai to machine driver
    Add an entry to sdp4430_dai. This entry glues HDMI cpu dai to
    a null dai with the same characteristics as HDMI dai. The null dai
    will be registered in machine driver's init function.
    
    Signed-off-by: Jorge Eduardo Candelaria <x0107209@ti.com>

--- sound/soc/omap/sdp4430.c
+++ sound/soc/omap/sdp4430.c
@@ -149,6 +149,20 @@ static int sdp4430_twl6030_init(struct snd_soc_codec *codec)
 	return ret;
 }
 
+#ifdef SND_OMAP_SOC_HDMI
+struct snd_so_dai null_dai = {
+	.name = "null",
+	.playback
+	{
+		.stream_name = "Playback",
+		.channels_min = 2,
+		.channels_max = 8,
+		.rates = SNDRV_PCM_RATE_48000,
+		.format = SNDRV_PCM_FORMAT_S32_LE,
+	},
+}
+#endif
+
 /* Digital audio interface glue - connects codec <--> CPU */
 static struct snd_soc_dai_link sdp4430_dai[] = {
 	{
@@ -182,6 +196,14 @@ static struct snd_soc_dai_link sdp4430_dai[] = {
 		.cpu_dai = &omap_abe_dai[OMAP_ABE_VIB_DAI],
 		.codec_dai = &abe_dai[4],
 	},
+#ifdef SND_OMAP_SOC_HDMI
+	{
+		.name = "hdmi",
+		.stream_name = "HDMI",
+		.cpu_dai = &omap_hdmi_dai,
+		.codec_dai = &null_dai,
+	},
+#endif
 };
 
 /* Audio machine driver */
@@ -209,6 +231,10 @@ static int __init sdp4430_soc_init(void)
 		return -ENODEV;
 	}
 	printk(KERN_INFO "SDP4430 SoC init\n");
+
+#ifdef SND_OMAP_SOC_HDMI
+	snd_soc_register_dais(&null_dai, 1);
+#endif
 
 	sdp4430_snd_device = platform_device_alloc("soc-audio", -1);
 	if (!sdp4430_snd_device) {
