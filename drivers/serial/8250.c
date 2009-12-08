0f1b306c29e15ecfc7cc2118a2a1c01c34999761
Vikram Pandita <vikram.pandita@ti.com>
2009-12-08 21:45:58 +0530

    omap: serial: fix non-empty uart fifo read abort
    OMAP3xxx and OMAP4430 UART IP blocks have a restriction wrt RX FIFO.
    Empty RX fifo read causes an abort.
    
    OMAP3xxx:
    	UART IP revision >= 0x52 have this issue
    	MVR register format is:
    	Bits  Field Name  Description  				Type  Reset
    	31:8   RESERVED 					RO  	0x0
    	7:4    MAJOR  	Major revision number of the module.  	RO  	0x--
    	3:0    MINOR  	Minor revision number of the module.  	RO  	0x--
    
    OMAP4xxx:
    	All revisions have this issue
    	Revision id check is not used as the format of MVR resigster has changed
    	For omap4 MVR register reads as: 0x50410602 => Revision id = 0x0602
    	Format of MVR register on omap4 is: (Courtesy: Cousson, Benoit)
    	Bits  Field Name  Description  				Type  Reset
    	31:30 SCHEME  	Scheme revision number of module  	RO  	0x1
    	29:28 RESERVED   					RO  	0x1
    	27:16 FUNC  	Function revision number of module  	RO  	0x041
    	15:11 RTL  		Rtl revision number of module  	RO  	0x00
    	10:8  MAJOR 	Major revision number of the module.  	RO  	0x6
    	7:6   CUSTOM  	Custom revision number of the module.  	RO  	0x0
    	5:0   MINOR  	Minor revision number of the module.  	RO  	0x02
    
    Override the default 8250 read handler: mem_serial_in()
    by a custom handler: serial_in_8250()
    which makes sure that RX fifo is not read when empty
    
    tested on zoom3(3630) board
    
    Cc: Cousson, Benoit <b-cousson@ti.com>
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>

--- drivers/serial/8250.c
+++ drivers/serial/8250.c
@@ -1243,8 +1243,7 @@ static void autoconfig(struct uart_8250_port *up, unsigned int probeflags)
 #endif
 	serial_outp(up, UART_MCR, save_mcr);
 	serial8250_clear_fifos(up);
-	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
-		serial_in(up, UART_RX);
+	serial_in(up, UART_RX);
 	if (up->capabilities & UART_CAP_UUE)
 		serial_outp(up, UART_IER, UART_IER_UUE);
 	else
@@ -1288,8 +1287,7 @@ static void autoconfig_irq(struct uart_8250_port *up)
 	}
 	serial_outp(up, UART_IER, 0x0f);	/* enable all intrs */
 	(void)serial_inp(up, UART_LSR);
-	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
-		(void)serial_inp(up, UART_RX);
+	(void)serial_inp(up, UART_RX);
 	(void)serial_inp(up, UART_IIR);
 	(void)serial_inp(up, UART_MSR);
 	serial_outp(up, UART_TX, 0xFF);
@@ -1984,8 +1982,7 @@ static int serial8250_startup(struct uart_port *port)
 	 * Clear the interrupt registers.
 	 */
 	(void) serial_inp(up, UART_LSR);
-	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
-		(void) serial_inp(up, UART_RX);
+	(void) serial_inp(up, UART_RX);
 	(void) serial_inp(up, UART_IIR);
 	(void) serial_inp(up, UART_MSR);
 
@@ -2142,8 +2139,7 @@ dont_test_tx_en:
 	 * routines or the previous session.
 	 */
 	serial_inp(up, UART_LSR);
-	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
-		serial_inp(up, UART_RX);
+	serial_inp(up, UART_RX);
 	serial_inp(up, UART_IIR);
 	serial_inp(up, UART_MSR);
 	up->lsr_saved_flags = 0;
@@ -2209,8 +2205,7 @@ static void serial8250_shutdown(struct uart_port *port)
 	 * Read data port to reset things, and then unlink from
 	 * the IRQ chain.
 	 */
-	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
-		(void) serial_in(up, UART_RX);
+	(void) serial_in(up, UART_RX);
 
 	del_timer_sync(&up->timer);
 	up->timer.function = serial8250_timeout;
