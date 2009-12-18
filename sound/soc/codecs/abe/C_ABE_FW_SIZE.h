4457afd301d55bd2fa2cb311aecc8dfa77a529f8
Margarita Olaya <x0080101@ti.com>
2009-12-18 16:00:35 -0600

    ASoC: ABE: Update coefficient to HAL0515
    Signed-off-by: Margarita Olaya <x0080101@ti.com>

--- sound/soc/codecs/abe/C_ABE_FW_SIZE.h
+++ sound/soc/codecs/abe/C_ABE_FW_SIZE.h
@@ -1,14 +1,5 @@
-/*
- * ==========================================================================
- *               Texas Instruments OMAP(TM) Platform Firmware
- * (c) Copyright 2009, Texas Instruments Incorporated.  All Rights Reserved.
- *
- *  Use of this firmware is controlled by the terms and conditions found
- *  in the license agreement under which this firmware has been supplied.
- * ==========================================================================
- */
+/* FW 05.10  MEMORY SIZES */
+#define ABE_DMEM_SIZE_OPTIMIZED	16384
+#define ABE_SMEM_SIZE_OPTIMIZED	16040
+#define ABE_CMEM_SIZE_OPTIMIZED	6552
 
-/* FW 05.10 MEMORY SIZES */
-#define ABE_DMEM_SIZE_OPTIMIZED		16384
-#define ABE_SMEM_SIZE_OPTIMIZED		14848
-#define ABE_CMEM_SIZE_OPTIMIZED		6552
