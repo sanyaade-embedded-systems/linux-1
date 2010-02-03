07f19b5a3a3f594964053e97970c0328a1fb650f
Misael Lopez Cruz <x0052729@ti.com>
2010-02-03 12:52:40 -0600

    ARM: OMAP4: TWL6040 codec platform data
    TWL6040 requires codec_enable function to turn codec on/off
    and naudint_irq to signal codec internal events.
    
    Signed-off-by: Misael Lopez Cruz <x0052729@ti.com>
    Signed-off-by: Margarita Olaya Cabrera <magi.olaya@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -476,6 +476,19 @@ static struct regulator_init_data sdp4430_vusb = {
 	},
 };
 
+static struct twl4030_codec_data twl6040_codec = {
+#ifdef CONFIG_OMAP4_AUDIO_PWRON
+        .audpwron_gpio  = 127,
+        .naudint_irq    = INT_44XX_SYS_NIRQ2,
+#else
+	/* provide GPIO number above the valid value
+	 * to mean there is no GPIO connected,
+	 * likewise do not provide any valid IRQ number */
+        .audpwron_gpio  = 1024,
+        .naudint_irq    = 0,
+#endif
+};
+
 static struct twl4030_madc_platform_data sdp4430_gpadc_data = {
 	.irq_line	= 1,
 };
@@ -500,6 +513,9 @@ static struct twl4030_platform_data sdp4430_twldata = {
 	.vaux3		= &sdp4430_vaux3,
 	.madc           = &sdp4430_gpadc_data,
 	.bci            = &sdp4430_bci_data,
+
+	/* children */
+	.codec		= &twl6040_codec,
 };
 
 static struct i2c_board_info __initdata sdp4430_i2c_boardinfo[] = {
