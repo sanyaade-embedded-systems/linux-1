86bbd2815aa0096ea7e7ec773d144f162588885b
Tony Lindgren <tony@atomide.com>
2010-02-15 09:27:25 -0800

    omap3/4: Fix compile for multi-omap for clkops_noncore_dpll_ops
    Rename clkops_noncore_dpll_ops for omap3 and omap4.
    
    Acked-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Acked-by: Paul Walmsley <paul@pwsan.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- arch/arm/mach-omap2/clock34xx.h
+++ arch/arm/mach-omap2/clock34xx.h
@@ -20,7 +20,7 @@ extern struct clk *arm_fck_p;
 extern const struct clkops clkops_omap3430es2_ssi_wait;
 extern const struct clkops clkops_omap3430es2_hsotgusb_wait;
 extern const struct clkops clkops_omap3430es2_dss_usbhost_wait;
-extern const struct clkops clkops_noncore_dpll_ops;
+extern const struct clkops omap3_clkops_noncore_dpll_ops;
 
 /* AM35xx-specific clkops */
 extern const struct clkops clkops_am35xx_ipss_module_wait;
