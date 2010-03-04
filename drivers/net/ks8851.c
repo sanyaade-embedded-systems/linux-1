7cc0b257746f3e48f1c8a81e8d1e25d253103b94
Maulik Mankad <x0082077@ti.com>
2010-03-04 12:02:07 -0600

    KS8851: Use dedicated work queue for ethernet.
    This patch fixes a softlock up issue seen when a
    USB drive is connected to the MUSB Host.
    
    The NFS filesystem could not be accessed and the
    ping of the board IP stops when this issue occurs.
    
    The issue gets fixed by introducing a dedicated
    work queue for the ethernet driver.
    
    Signed-off-by: Maulik Mankad <x0082077@ti.com>
    Signed-off-by: Ricardo Perez Olivares <x0081762@ti.com>
    Signed-off-by: Manjunatha GK <manjugk@ti.com>

--- drivers/net/ks8851.c
+++ drivers/net/ks8851.c
@@ -123,6 +123,8 @@ struct ks8851_net {
 	struct spi_transfer	spi_xfer2[2];
 };
 
+struct workqueue_struct *eth_wq;
+
 static int msg_enable;
 
 #define ks_info(_ks, _msg...) dev_info(&(_ks)->spidev->dev, _msg)
@@ -397,7 +399,7 @@ static irqreturn_t ks8851_irq(int irq, void *pw)
 	struct ks8851_net *ks = pw;
 
 	disable_irq_nosync(irq);
-	schedule_work(&ks->irq_work);
+	queue_work(eth_wq, &ks->irq_work);
 	return IRQ_HANDLED;
 }
 
@@ -921,7 +923,7 @@ static netdev_tx_t ks8851_start_xmit(struct sk_buff *skb,
 	}
 
 	spin_unlock(&ks->statelock);
-	schedule_work(&ks->tx_work);
+	queue_work(eth_wq,&ks->tx_work);
 
 	return ret;
 }
@@ -999,7 +1001,7 @@ static void ks8851_set_rx_mode(struct net_device *dev)
 
 	if (memcmp(&rxctrl, &ks->rxctrl, sizeof(rxctrl)) != 0) {
 		memcpy(&ks->rxctrl, &rxctrl, sizeof(ks->rxctrl));
-		schedule_work(&ks->rxctrl_work);
+		queue_work(eth_wq, &ks->rxctrl_work);
 	}
 
 	spin_unlock(&ks->statelock);
@@ -1228,6 +1230,8 @@ static int __devinit ks8851_probe(struct spi_device *spi)
 
 	mutex_init(&ks->lock);
 	spin_lock_init(&ks->statelock);
+
+	eth_wq = create_workqueue("eth_workqueue");
 
 	INIT_WORK(&ks->tx_work, ks8851_tx_work);
 	INIT_WORK(&ks->irq_work, ks8851_irq_work);
