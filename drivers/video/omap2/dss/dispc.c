e6eb8451cb3f5076590aae1e34ad3ccde104f6c0
Mayuresh Janorkar <mayur@ti.com>
2010-03-22 11:58:12 +0530

    OMAP4 dispc base address Changes the dispc base address for OMAP4
    Signed-off-by: Mayuresh Janorkar <mayur@ti.com>
    Signed-off-by: Senthilvadivu Guruswamy <svadivu@ti.com>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -39,8 +39,11 @@
 
 #include "dss.h"
 
-/* DISPC */
-#define DISPC_BASE			0x48050400
+#ifndef CONFIG_ARCH_OMAP4
+#define DISPC_BASE		0x48050400
+#else
+#define DISPC_BASE		0x58001000
+#endif
 
 #define DISPC_SZ_REGS			SZ_1K
 
