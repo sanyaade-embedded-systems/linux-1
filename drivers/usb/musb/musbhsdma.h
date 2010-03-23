audioscience/linux-omap3

musb: fix Inventra DMA lockup issue
MUSB RTL version 1.4 has a hardware issue when TX and RX DMA
channels are simultaneously enabled. To work around this issue,
use System DMA for all RX channels.

System DMA performance is lower than Inventra DMA and thus Rx
throughput would be lower.

Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

drivers/usb/musb/musbhsdma.h
@@ -35,6 +35,21 @@
 #include "omap2430.h"
 #endif
 
+#if defined(CONFIG_ARCH_OMAP2430) || defined(CONFIG_ARCH_OMAP3430)
+#include "omap2430.h"
+#endif
+
+#ifdef CONFIG_MUSB_USE_SYSTEM_DMA_RX
+static int use_sdma = 1;
+#else
+#define use_sdma          0
+#endif
+
+int use_system_dma(void)
+{
+	return use_sdma;
+}
+
 #ifndef CONFIG_BLACKFIN
 
 #define MUSB_HSDMA_BASE		0x200
@@ -125,6 +140,9 @@ static inline void musb_write_hsdma_count(void __iomem *mbase,
 
 #define MUSB_HSDMA_CHANNELS		8
 
+#define MUSB_FIFO_ADDRESS(epnum)      \
+	((unsigned long) (OMAP_HSOTG_BASE + MUSB_FIFO_OFFSET(epnum)))
+
 struct musb_dma_controller;
 
 struct musb_dma_channel {
@@ -136,6 +154,7 @@ struct musb_dma_channel {
 	u8				idx;
 	u8				epnum;
 	u8				transmit;
+	int                             sysdma_channel;
 };
 
 struct musb_dma_controller {

sanyaade-embedded-systems/linux-1

drivers/usb/musb/musbhsdma.h
@@ -35,6 +35,21 @@
 #include "omap2430.h"
 #endif
 
+#if defined(CONFIG_ARCH_OMAP2430) || defined(CONFIG_ARCH_OMAP3430)
+#include "omap2430.h"
+#endif
+
+#ifdef CONFIG_MUSB_USE_SYSTEM_DMA_RX
+static int use_sdma = 1;
+#else
+#define use_sdma          0
+#endif
+
+int use_system_dma(void)
+{
+	return use_sdma;
+}
+
 #ifndef CONFIG_BLACKFIN
 
 #define MUSB_HSDMA_BASE		0x200
@@ -142,6 +157,9 @@ static inline void musb_write_hsdma_count(void __iomem *mbase,
 
 #define MUSB_HSDMA_CHANNELS		8
 
+#define MUSB_FIFO_ADDRESS(epnum)      \
+	((unsigned long) (OMAP34XX_HSUSB_OTG_BASE + MUSB_FIFO_OFFSET(epnum)))
+
 struct musb_dma_controller;
 
 struct musb_dma_channel {
@@ -153,6 +171,7 @@ struct musb_dma_channel {
 	u8				idx;
 	u8				epnum;
 	u8				transmit;
+	int                             sysdma_channel;
 };
 
 struct musb_dma_controller {