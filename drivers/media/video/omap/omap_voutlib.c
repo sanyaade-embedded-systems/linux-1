788c1e3826e1b4f6e3153674894b9ed6a653b5f1
Lajos Molnar <molnar@ti.com>
2010-03-30 20:43:09 +0530

    V4L2: Added frame-synchronized crop support to TI DSS v4l2 driver.
    Now each buffer remembers the crop settings at QBUF time, and uses
    that information when displaying that buffers.  This allows implementing
    0-copy VSTAB using crop regions.
    
    Also corrected OMAP2 and OMAP3 limitations to downscaling that do not
    apply for OMAP4.
    
    Also corrected spacing issues in omap_vout_isr.
    
    Signed-off-by: Lajos Molnar <molnar@ti.com>

--- drivers/media/video/omap/omap_voutlib.c
+++ drivers/media/video/omap/omap_voutlib.c
@@ -23,6 +23,7 @@
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/videodev2.h>
+#include <plat/cpu.h>
 
 MODULE_AUTHOR("Texas Instruments.");
 MODULE_DESCRIPTION("OMAP Video library");
@@ -172,9 +173,9 @@ int omap_vout_new_crop(struct v4l2_pix_format *pix,
 	if (try_crop.width <= 0 || try_crop.height <= 0)
 		return -EINVAL;
 
-	if (crop->height != win->w.height) {
+	if (cpu_is_omap24xx() && crop->height != win->w.height) {
 		/* If we're resizing vertically, we can't support a crop width
-		 * wider than 768 pixels.
+		 * wider than 768 pixels on OMAP2.
 		 */
 		if (try_crop.width > 768)
 			try_crop.width = 768;
