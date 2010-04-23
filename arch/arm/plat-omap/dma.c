0fe4b401dee51a0116202da4e8e0e7f3b784864c
Philip Balister <philip@opensdr.com>
2010-04-22 19:41:58 -0700

    Add functions to dma.c to set address and length for src and dest.

--- arch/arm/plat-omap/dma.c
+++ arch/arm/plat-omap/dma.c
@@ -471,6 +471,21 @@ void omap_set_dma_src_burst_mode(int lch, enum omap_dma_burst_mode burst_mode)
 }
 EXPORT_SYMBOL(omap_set_dma_src_burst_mode);
 
+void omap_set_dma_src_addr_size(int lch, unsigned int addr, int elem_count)
+{
+
+	if (cpu_class_is_omap1()) {
+		p->dma_write(addr >> 16, CSSA, lch);
+		//p->dma_write((u16)addr, CSSA_L, lch);
+	}
+
+	if (cpu_class_is_omap2())
+		p->dma_write(addr, CSSA, lch);
+
+	p->dma_write(elem_count, CEN, lch);
+}
+EXPORT_SYMBOL_GPL(omap_set_dma_src_addr_size);
+
 /* Note that dest_port is only for OMAP1 */
 void omap_set_dma_dest_params(int lch, int dest_port, int dest_amode,
 			      unsigned long dest_start,
@@ -560,6 +575,21 @@ void omap_set_dma_dest_burst_mode(int lch, enum omap_dma_burst_mode burst_mode)
 	p->dma_write(l, CSDP, lch);
 }
 EXPORT_SYMBOL(omap_set_dma_dest_burst_mode);
+
+void omap_set_dma_dest_addr_size(int lch, unsigned int addr, int elem_count)
+{
+
+	if (cpu_class_is_omap1()) {
+		p->dma_write(addr >> 16, CDSA, lch);
+		//p->dma_write((u16)addr, CDSA_L, lch);
+	}
+
+	if (cpu_class_is_omap2())
+		p->dma_write(addr, CDSA, lch);
+
+	p->dma_write(elem_count, CEN, lch);
+}
+EXPORT_SYMBOL_GPL(omap_set_dma_dest_addr_size);
 
 static inline void omap_enable_channel_irq(int lch)
 {
