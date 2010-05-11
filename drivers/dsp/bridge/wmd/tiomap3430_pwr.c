b7daad90819d58f3706712e57bce97c6e39b97d3
Omar Ramirez Luna <omar.ramirez@ti.com>
2010-05-11 16:50:53 -0500

    DSPBRIDGE: Remove success codes
    Change to return 0 (zero) on any success scenario,
    except DSP_SENUMCOMPLETE.
    
    These are no longer used:
    DSP_SOK
    DSP_SALREADYATTACHED
    DSP_SALREADYASLEEP
    DSP_SALREADYAWAKE
    DSP_STRUE
    DSP_SFALSE
    DSP_SNODEPENDENTLIBS
    DSP_SALREADYLOADED
    DSP_OKTO_CONTINUE
    
    Signed-off-by: Omar Ramirez Luna <omar.ramirez@ti.com>

--- drivers/dsp/bridge/wmd/tiomap3430_pwr.c
+++ drivers/dsp/bridge/wmd/tiomap3430_pwr.c
@@ -77,7 +77,7 @@ dsp_status handle_constraints_set(struct wmd_dev_context *dev_context,
 	if (pdata->dsp_set_min_opp)
 		(*pdata->dsp_set_min_opp) (opp_idx);
 #endif /* #ifdef CONFIG_BRIDGE_DVFS */
-	return DSP_SOK;
+	return 0;
 }
 
 /*
@@ -86,7 +86,7 @@ dsp_status handle_constraints_set(struct wmd_dev_context *dev_context,
  */
 dsp_status handle_hibernation_from_dsp(struct wmd_dev_context *dev_context)
 {
-	dsp_status status = DSP_SOK;
+	dsp_status status = 0;
 #ifdef CONFIG_PM
 	u16 timeout = PWRSTST_TIMEOUT / 10;
 	u32 pwr_state;
@@ -156,7 +156,7 @@ func_cont:
 			 */
 			if (pdata->dsp_set_min_opp)
 				(*pdata->dsp_set_min_opp) (VDD1_OPP1);
-			status = DSP_SOK;
+			status = 0;
 		}
 #endif /* CONFIG_BRIDGE_DVFS */
 	}
@@ -171,7 +171,7 @@ func_cont:
 dsp_status sleep_dsp(struct wmd_dev_context *dev_context, IN u32 dw_cmd,
 		     IN void *pargs)
 {
-	dsp_status status = DSP_SOK;
+	dsp_status status = 0;
 #ifdef CONFIG_PM
 #ifdef CONFIG_BRIDGE_NTFY_PWRERR
 	struct deh_mgr *hdeh_mgr;
@@ -205,17 +205,17 @@ dsp_status sleep_dsp(struct wmd_dev_context *dev_context, IN u32 dw_cmd,
 			sm_interrupt_dsp(dev_context, MBX_PM_DSPHIBERNATE);
 			target_pwr_state = PWRDM_POWER_OFF;
 		} else
-			return DSP_SOK;
+			return 0;
 		break;
 	case BRD_HIBERNATION:
 	case BRD_DSP_HIBERNATION:
 		/* Already in Hibernation, so just return */
 		dev_dbg(bridge, "PM: %s - DSP already in hibernation\n",
 			__func__);
-		return DSP_SOK;
+		return 0;
 	case BRD_STOPPED:
 		dev_dbg(bridge, "PM: %s - Board in STOP state\n", __func__);
-		return DSP_SALREADYASLEEP;
+		return 0;
 	default:
 		dev_dbg(bridge, "PM: %s - Bridge in Illegal state\n", __func__);
 		return -EPERM;
@@ -296,7 +296,7 @@ func_cont:
  */
 dsp_status wake_dsp(struct wmd_dev_context *dev_context, IN void *pargs)
 {
-	dsp_status status = DSP_SOK;
+	dsp_status status = 0;
 #ifdef CONFIG_PM
 
 	/* Check the BRD/WMD state, if it is not 'SLEEP' then return failure */
@@ -304,7 +304,7 @@ dsp_status wake_dsp(struct wmd_dev_context *dev_context, IN void *pargs)
 	    dev_context->dw_brd_state == BRD_STOPPED) {
 		/* The Device is in 'RET' or 'OFF' state and WMD state is not
 		 * 'SLEEP', this means state inconsistency, so return */
-		return DSP_SOK;
+		return 0;
 	}
 
 	/* Send a wakeup message to DSP */
@@ -326,8 +326,8 @@ dsp_status dsp_peripheral_clk_ctrl(struct wmd_dev_context *dev_context,
 	u32 clk_id_index = MBX_PM_MAX_RESOURCES;
 	u32 tmp_index;
 	u32 dsp_per_clks_before;
-	dsp_status status = DSP_SOK;
-	dsp_status status1 = DSP_SOK;
+	dsp_status status = 0;
+	dsp_status status1 = 0;
 	struct cfg_hostres *resources = dev_context->resources;
 	u32 value;
 
@@ -435,17 +435,17 @@ dsp_status pre_scale_dsp(struct wmd_dev_context *dev_context, IN void *pargs)
 	    (dev_context->dw_brd_state == BRD_DSP_HIBERNATION)) {
 		dev_dbg(bridge, "OPP: %s IVA in sleep. No message to DSP\n",
 			__func__);
-		return DSP_SOK;
+		return 0;
 	} else if ((dev_context->dw_brd_state == BRD_RUNNING)) {
 		/* Send a prenotificatio to DSP */
 		dev_dbg(bridge, "OPP: %s sent notification to DSP\n", __func__);
 		sm_interrupt_dsp(dev_context, MBX_PM_SETPOINT_PRENOTIFY);
-		return DSP_SOK;
+		return 0;
 	} else {
 		return -EPERM;
 	}
 #endif /* #ifdef CONFIG_BRIDGE_DVFS */
-	return DSP_SOK;
+	return 0;
 }
 
 /*
@@ -455,7 +455,7 @@ dsp_status pre_scale_dsp(struct wmd_dev_context *dev_context, IN void *pargs)
  */
 dsp_status post_scale_dsp(struct wmd_dev_context *dev_context, IN void *pargs)
 {
-	dsp_status status = DSP_SOK;
+	dsp_status status = 0;
 #ifdef CONFIG_BRIDGE_DVFS
 	u32 level;
 	u32 voltage_domain;
@@ -498,7 +498,7 @@ dsp_status dsp_peripheral_clocks_disable(struct wmd_dev_context *dev_context,
 					 IN void *pargs)
 {
 	u32 clk_idx;
-	dsp_status status = DSP_SOK;
+	dsp_status status = 0;
 	struct cfg_hostres *resources = dev_context->resources;
 	u32 value;
 
@@ -574,15 +574,15 @@ dsp_status dsp_peripheral_clocks_enable(struct wmd_dev_context *dev_context,
 			    services_clk_enable(bpwr_clks[clk_idx].fun_clk);
 		}
 	}
-	if ((int_clk_status | fun_clk_status) != DSP_SOK)
+	if ((int_clk_status | fun_clk_status) != 0)
 		return -EPERM;
-	return DSP_SOK;
+	return 0;
 }
 
 void dsp_clk_wakeup_event_ctrl(u32 ClkId, bool enable)
 {
 	struct cfg_hostres *resources;
-	dsp_status status = DSP_SOK;
+	dsp_status status = 0;
 	u32 iva2_grpsel;
 	u32 mpu_grpsel;
 	struct dev_object *hdev_object = NULL;
