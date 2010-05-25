ffb88eeeb315d53546ac2220c948721311758fd4
Mythri P K <mythripk@ti.com>
2010-05-25 16:55:01 +0530

    OMAP4 : HDMI driver changes to support Hot Plug Detect
    Signed-off-by: Mythri P K <mythripk@ti.com>

--- drivers/video/omap2/dss/hdmi.h
+++ drivers/video/omap2/dss/hdmi.h
@@ -230,6 +230,11 @@ static int hdmi_read_edid(struct omap_video_timings *);
 static int get_edid_timing_data(u8 *edid, u16 *pixel_clk, u16 *horizontal_res,
 			  u16 *vertical_res);
 void show_horz_vert_timing_info(u8 *edid);
+int hdmi_get_image_format(void);
+int hdmi_get_audio_format(void);
+static irqreturn_t hdmi_irq_handler();
+static int hdmi_enable_hpd(struct omap_dss_device *dssdev);
+static void hdmi_power_off(struct omap_dss_device *dssdev);
 
 #ifdef __cplusplus
 };
