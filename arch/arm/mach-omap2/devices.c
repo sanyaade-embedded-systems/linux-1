65600bb1b23a0d5aef50292ce175e554d26d4ed0
Archit Taneja <archit@ti.com>
2010-06-01 18:11:36 +0530

    OMAP4: Add wb device in devices.c
    Signed-off-by: Archit Taneja <archit@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -822,8 +822,25 @@ static void omap_init_vout(void)
 	if (platform_device_register(&omap_vout_device) < 0)
 		printk(KERN_ERR "Unable to register OMAP-VOUT device\n");
 }
+
+static struct resource sdp4430_wb_resource[1] = {
+};
+
+static struct platform_device sdp4430_wb_device = {
+	.name		= "omap_wb",
+	.num_resources	= ARRAY_SIZE(sdp4430_wb_resource),
+	.resource	= &sdp4430_wb_resource[0],
+	.id		= -1,
+};
+
+static void omap_init_wb(void)
+{
+		(void) platform_device_register(&sdp4430_wb_device);
+}
+
 #else
 static inline void omap_init_vout(void) {}
+static void omap_init_wb(void) {}
 #endif
 
 /*-------------------------------------------------------------------------*/
@@ -841,6 +858,8 @@ static int __init omap2_init_devices(void)
 	omap_init_sti();
 	omap_init_sha1_md5();
 	omap_init_vout();
+	if (cpu_is_omap44xx())
+		omap_init_wb();
 
 	return 0;
 }
