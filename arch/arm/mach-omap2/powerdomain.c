1bf78dc8ccaac6d6daa7c0f800cbb6cbc09ea84d
Rajendra Nayak <rnayak@ti.com>
2010-06-16 08:28:02 -0500

    OMAP4: powerdomain: Add support to read previous pwst entered
    OMAP4430 ES2 has additional bitfields in PWRSTST register which help
    identify the previous power state entered by the powerdomain.
    Update pwrdm_read_prev_pwrst api to support this.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/powerdomain.c
+++ arch/arm/mach-omap2/powerdomain.c
@@ -494,8 +494,12 @@ int pwrdm_read_prev_pwrst(struct powerdomain *pwrdm)
 	if (!pwrdm)
 		return -EINVAL;
 
-	return prm_read_mod_bits_shift(pwrdm->prcm_offs, OMAP3430_PM_PREPWSTST,
-					OMAP3430_LASTPOWERSTATEENTERED_MASK);
+	if (cpu_is_omap24xx() || cpu_is_omap34xx())
+		return prm_read_mod_bits_shift(pwrdm->prcm_offs, OMAP3430_PM_PREPWSTST,
+						OMAP3430_LASTPOWERSTATEENTERED_MASK);
+	else if (cpu_is_omap44xx())
+		return prm_read_mod_bits_shift(pwrdm->prcm_offs, OMAP4_PM_PWSTST,
+						OMAP4430_LASTPOWERSTATEENTERED_MASK);
 }
 
 /**
