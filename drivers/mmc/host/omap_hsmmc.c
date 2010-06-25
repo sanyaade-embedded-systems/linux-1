bc3634422ba1c27b20b17ea33289eeb77c7d075d
Panduranga <x0125717@ti.com>
2010-06-25 11:38:50 +0530

    OMAP4: mmc: host: wl1283: Support for MMC5 controller TI WLAN
       added support for MMC5 controller to notify
       status and registration details.
    
    Signed-off-by: Panduranga <x0125717@ti.com>

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -1621,6 +1621,30 @@ static void omap_hsmmc_set_ios(struct mmc_host *mmc, struct mmc_ios *ios)
 		mmc_host_lazy_disable(host->mmc);
 }
 
+#ifdef CONFIG_TIWLAN_SDIO
+static void omap_hsmmc_status_notify_cb(int card_present, void *dev_id)
+{
+       struct omap_hsmmc_host *host = dev_id;
+       struct omap_mmc_slot_data *slot = &mmc_slot(host);
+       int carddetect;
+
+       printk(KERN_DEBUG "%s: card_present %d\n", mmc_hostname(host->mmc),
+                card_present);
+
+       carddetect = slot->card_detect(host->dev, host->slot_id);
+
+       sysfs_notify(&host->mmc->class_dev.kobj, NULL, "cover_switch");
+        if (carddetect) {
+                mmc_detect_change(host->mmc, (HZ * 200) / 1000);
+        } else {
+                mmc_host_enable(host->mmc);
+                omap_hsmmc_reset_controller_fsm(host, SRD);
+                mmc_host_lazy_disable(host->mmc);
+                mmc_detect_change(host->mmc, (HZ * 50) / 1000);
+        }
+}
+#endif
+
 static int omap_hsmmc_get_cd(struct mmc_host *mmc)
 {
 	struct omap_hsmmc_host *host = mmc_priv(mmc);
@@ -2159,6 +2183,17 @@ static int __init omap_hsmmc_probe(struct platform_device *pdev)
 	host->base	= ioremap(host->mapbase, SZ_4K);
 	host->power_mode = MMC_POWER_OFF;
 
+#ifdef CONFIG_TIWLAN_SDIO
+        if (pdev->id == CONFIG_TIWLAN_MMC_CONTROLLER-1) {
+                if (pdata->slots[0].embedded_sdio != NULL) {
+                        mmc_set_embedded_sdio_data(mmc,
+                        &pdata->slots[0].embedded_sdio->cis,
+                        pdata->slots[0].embedded_sdio->funcs,
+			pdata->slots[0].embedded_sdio->quirks);
+                }
+        }
+#endif
+
 	platform_set_drvdata(pdev, host);
 	INIT_WORK(&host->mmc_carddetect_work, omap_hsmmc_detect);
 	if (mmc_slot(host).power_saving) {
@@ -2302,6 +2337,15 @@ static int __init omap_hsmmc_probe(struct platform_device *pdev)
 		pdata->suspend = omap_hsmmc_suspend_cdirq;
 		pdata->resume = omap_hsmmc_resume_cdirq;
 	}
+
+#ifdef CONFIG_TIWLAN_SDIO
+        else if (mmc_slot(host).register_status_notify) {
+                if (pdev->id == CONFIG_TIWLAN_MMC_CONTROLLER-1) {
+                        mmc_slot(host).register_status_notify(
+                                omap_hsmmc_status_notify_cb, host);
+                }
+        }
+#endif
 
 	OMAP_HSMMC_WRITE(host->base, ISE, INT_EN_MASK);
 	OMAP_HSMMC_WRITE(host->base, IE, INT_EN_MASK);
