KFire-Android/kernel_omap_otter-common

omap4: l2x0: Set share override bit of Pl310
Clearing bit 22 in the PL310 Auxiliary Control register (shared
attribute override enable) has the side effect of transforming Normal
Shared Non-cacheable reads into Cacheable no-allocate reads.

Coherent DMA buffers in Linux always have a Cacheable alias via the
kernel linear mapping and the processor can speculatively load cache
lines into the PL310 controller. With bit 22 cleared, Non-cacheable
reads would unexpectedly hit such cache lines leading to buffer
corruption

Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>

arch/arm/mach-omap2/omap4-common.c
@@ -212,6 +212,9 @@ static int __init omap_l2_cache_init(void)
 	l2cache_base = ioremap(OMAP44XX_L2CACHE_BASE, SZ_4K);
 	BUG_ON(!l2cache_base);
 
+	if (omap_rev() != OMAP4430_REV_ES1_0)
+		omap_smc1(0x109, 0x0e470000);
+
 	/* Enable PL310 L2 Cache controller */
 	omap_smc1(0x102, 0x1);
 
@@ -219,10 +222,10 @@ static int __init omap_l2_cache_init(void)
 	 * 32KB way size, 16-way associativity,
 	 * parity disabled
 	 */
-	if (omap_rev() >= OMAP4430_REV_ES2_0)
-		l2x0_init(l2cache_base, 0x0e070000, 0xc0000fff);
-	else
+	if (omap_rev() == OMAP4430_REV_ES1_0)
 		l2x0_init(l2cache_base, 0x0e050000, 0xc0000fff);
+	else
+		l2x0_init(l2cache_base, 0x0e470000, 0xc0000fff);
 
 	return 0;
 }

sanyaade-embedded-systems/linux-1

arch/arm/mach-omap2/omap4-common.c
@@ -211,6 +211,9 @@ static int __init omap_l2_cache_init(void)
 	l2cache_base = ioremap(OMAP44XX_L2CACHE_BASE, SZ_4K);
 	BUG_ON(!l2cache_base);
 
+	if (omap_rev() == OMAP4430_REV_ES2_0)
+		omap_smc1(0x109, 0x0e470000);
+	
 	/* Enable PL310 L2 Cache controller */
 	omap_smc1(0x102, 0x1);
 
@@ -219,7 +222,7 @@ static int __init omap_l2_cache_init(void)
 	 * parity disabled
 	 */
 	if (omap_rev() == OMAP4430_REV_ES2_0)
-		l2x0_init(l2cache_base, 0x0e070000, 0xc0000fff);
+		l2x0_init(l2cache_base, 0x0e470000, 0xc0000fff);
 	else
 		l2x0_init(l2cache_base, 0x0e050000, 0xc0000fff);
 