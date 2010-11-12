917a4e143b9e70507bb3262ea2450041c7bebf12
Panduranga Mallireddy <x0125717@ti.com>
2010-11-13 02:09:06 +0530

    wl1271: Release interrupt *after* releasing the driver context
    The current order causes driver releasing to fail in various ways, and
    causes possible instability.
    
    Signed-off-by: Juuso Oikarinen <juuso.oikarinen@nokia.com>
    Reviewed-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: Luciano Coelho <luciano.coelho@nokia.com>
    Signed-off-by: Panduranga Mallireddy <x0125717@ti.com>

--- drivers/net/wireless/wl12xx/wl1271_sdio.c
+++ drivers/net/wireless/wl12xx/wl1271_sdio.c
@@ -287,9 +287,8 @@ static void __devexit wl1271_remove(struct sdio_func *func)
 	/* Undo decrement done above in wl1271_probe */
 	pm_runtime_get_noresume(&func->dev);
 
-	free_irq(wl->irq, wl);
-
 	wl1271_unregister_hw(wl);
+	free_irq(wl->irq, wl);
 	wl1271_free_hw(wl);
 }
 
