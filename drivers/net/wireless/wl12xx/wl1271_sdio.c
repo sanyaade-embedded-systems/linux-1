6f63def3de3d0935d3752dcc2888ce44ff50f99f
Panduranga Mallireddy <x0125717@ti.com>
2010-11-17 11:17:25 +0530

    Revert "wl1271: Release interrupt *after* releasing the driver context"
    This reverts commit 917a4e143b9e70507bb3262ea2450041c7bebf12.

--- drivers/net/wireless/wl12xx/wl1271_sdio.c
+++ drivers/net/wireless/wl12xx/wl1271_sdio.c
@@ -287,8 +287,9 @@ static void __devexit wl1271_remove(struct sdio_func *func)
 	/* Undo decrement done above in wl1271_probe */
 	pm_runtime_get_noresume(&func->dev);
 
-	wl1271_unregister_hw(wl);
 	free_irq(wl->irq, wl);
+
+	wl1271_unregister_hw(wl);
 	wl1271_free_hw(wl);
 }
 
