dd053e0d53b36e2160584921a78b910e05cb455f
Panduranga Mallireddy <x0125717@ti.com>
2010-11-17 11:06:51 +0530

    Revert "mmc: sdio: ensure mmc_sdio_detect is powered"
    This reverts commit e270e425cc1cfb204f4071d49ef52297d4f69269.

--- drivers/mmc/core/sdio.c
+++ drivers/mmc/core/sdio.c
@@ -436,11 +436,6 @@ static void mmc_sdio_detect(struct mmc_host *host)
 	BUG_ON(!host);
 	BUG_ON(!host->card);
 
-	/* Make sure card is powered before detecting it */
-	err = pm_runtime_get_sync(&host->card->dev);
-	if (err < 0)
-		goto out;
-
 	mmc_claim_host(host);
 
 	/*
@@ -450,7 +445,6 @@ static void mmc_sdio_detect(struct mmc_host *host)
 
 	mmc_release_host(host);
 
-out:
 	if (err) {
 		mmc_sdio_remove(host);
 
@@ -458,9 +452,6 @@ out:
 		mmc_detach_bus(host);
 		mmc_release_host(host);
 	}
-
-	/* Tell PM core that we're done */
-	pm_runtime_put(&host->card->dev);
 }
 
 /*
