d20d12bcceacabdeb7f2dad880e69aba14300cdf
Mayuresh Janorkar <mayur@ti.com>
2010-11-22 19:17:24 +0530

    Fixing crash during HDMI as default display
    A crash was observed when HDMI is used as a default display.
    
    In Panda board HDMI is a default device so this patch is critical for the Panda users.
    .enable for hdmi driver was removed and adding that has helped in solving the crash
    and a successful bootup.
    
    Signed-off-by: Mayuresh Janorkar <mayur@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -729,6 +729,7 @@ static struct omap_dss_driver hdmi_driver = {
 	.probe		= hdmi_panel_probe,
 	.remove		= hdmi_panel_remove,
 
+	.enable		= hdmi_panel_enable,
 	.disable	= hdmi_panel_disable,
 	.smart_enable	= hdmi_panel_enable,
 	.smart_is_enabled	= hdmi_panel_is_enabled,
