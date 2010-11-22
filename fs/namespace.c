4c7dfe34f85c51b2f9ee7a74bb8e1d612fc74a60
Andy Whitcroft <apw@canonical.com>
2010-11-22 15:04:45 +0000

    Revert "UBUNTU: ubuntu: AUFS -- aufs2-standalone.patch aufs2.1-36-UNRELEASED-20101103"
    This reverts commit 39e47347774cd894b19cb173770081473c97e2c2.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1321,7 +1321,6 @@ int iterate_mounts(int (*f)(struct vfsmount *, void *), void *arg,
 	}
 	return 0;
 }
-EXPORT_SYMBOL_GPL(iterate_mounts);
 
 static void cleanup_group_ids(struct vfsmount *mnt, struct vfsmount *end)
 {
