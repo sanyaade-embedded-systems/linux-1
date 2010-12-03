3d57ed1f9663779440645628294349bd32d1ae05
Sebastien Guiriec <s-guiriec@ti.com>
2010-12-03 22:16:58 +0100

    ASoC: ABE DSP: Update Mixer switch in order to enable/dissable the ABE gains
    Mixer switch controls are not associated to any gain controls. ABE
    enable/disable functions should be called when the switch is modified.
    
    Signed-off-by: Sebastien Guiriec <s-guiriec@ti.com>

--- sound/soc/omap/omap-abe-dsp.c
+++ sound/soc/omap/omap-abe-dsp.c
@@ -420,9 +420,11 @@ static int dl1_put_mixer(struct snd_kcontrol *kcontrol,
 	if (ucontrol->value.integer.value[0]) {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 1);
+		abe_enable_gain(MIXDL1, mc->reg);
 	} else {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 0);
+		abe_disable_gain(MIXDL1, mc->reg);
 	}
 	pm_runtime_put_sync(&pdev->dev);
 
@@ -444,9 +446,11 @@ static int dl2_put_mixer(struct snd_kcontrol *kcontrol,
 	if (ucontrol->value.integer.value[0]) {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 1);
+		abe_enable_gain(MIXDL2, mc->reg);
 	} else {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 0);
+		abe_disable_gain(MIXDL2, mc->reg);
 	}
 
 	pm_runtime_put_sync(&pdev->dev);
@@ -468,9 +472,11 @@ static int audio_ul_put_mixer(struct snd_kcontrol *kcontrol,
 	if (ucontrol->value.integer.value[0]) {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 1);
+		abe_enable_gain(MIXAUDUL, mc->reg);
 	} else {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 0);
+		abe_disable_gain(MIXAUDUL, mc->reg);
 	}
 	pm_runtime_put_sync(&pdev->dev);
 
@@ -492,9 +498,11 @@ static int vxrec_put_mixer(struct snd_kcontrol *kcontrol,
 	if (ucontrol->value.integer.value[0]) {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 1);
+		abe_enable_gain(MIXVXREC, mc->reg);
 	} else {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 0);
+		abe_disable_gain(MIXVXREC, mc->reg);
 	}
 	pm_runtime_put_sync(&pdev->dev);
 
@@ -516,9 +524,11 @@ static int sdt_put_mixer(struct snd_kcontrol *kcontrol,
 	if (ucontrol->value.integer.value[0]) {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 1);
+		abe_enable_gain(MIXSDT, mc->reg);
 	} else {
 		abe->dapm[mc->shift] = ucontrol->value.integer.value[0];
 		snd_soc_dapm_mixer_update_power(widget, kcontrol, 0);
+		abe_disable_gain(MIXSDT, mc->reg);
 	}
 	pm_runtime_put_sync(&pdev->dev);
 
