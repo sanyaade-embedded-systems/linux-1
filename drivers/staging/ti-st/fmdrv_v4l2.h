0670757f486a8b6cb24851851eae4a4a23de8908
Manjunatha Halli <manjunatha_halli@ti.com>
2011-01-12 13:01:01 +0530

    driver:staging: ti-st: make fm_v4l2 to be align with V10 FM driver
    This patch makes the L24 FM driver to have the same code as
    version V10 of FM V4L2 driver which is submitted to open source.
    
    Following are the comments which are taken care in this release,
      - using the 'early return' constructs where applicable.
      - Renaming all the variables based on scope and frequency of use.
      - Use of 'temp' pointers wherever required insted of breaking the lines.
      - printing the error messages at the lowest level,and just propagate the
      error code up without repeating the error message.
      - Remove use of 'break' after 'return' statement in few switch case
      statements.
      - Remove some unnecessary checks in fmdrv_common and fmdrv_rx
    
    Signed-off-by: Manjunatha Halli <manjunatha_halli@ti.com>

--- drivers/staging/ti-st/fmdrv_v4l2.h
+++ drivers/staging/ti-st/fmdrv_v4l2.h
@@ -3,7 +3,7 @@
  *
  *  FM V4L2 module header.
  *
- *  Copyright (C) 2010 Texas Instruments
+ *  Copyright (C) 2011 Texas Instruments
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
@@ -23,10 +23,10 @@
 #ifndef _FMDRV_V4L2_H
 #define _FMDRV_V4L2_H
 
-#include <media/v4l2-common.h>
 #include <media/v4l2-ioctl.h>
+#include <media/v4l2-common.h>
 
-int fm_v4l2_init_video_device(struct fmdrv_ops *, int);
+int fm_v4l2_init_video_device(struct fmdev *, int);
 void *fm_v4l2_deinit_video_device(void);
 
 #endif
