b09ffcb11626086fe956ed424aaa539846de9042
Vishwanath BS <vishwanath.bs@ti.com>
2011-03-30 11:56:44 +0530

    OMAP3+: PM: DVFS: introduce helper functions for DVFS
    This patch introduces helper functions for Device Voltage and Frequency
    Scaling (DVFS).
    Data structures added:
    1. omap_dev_user_list: This structure maintains list of frequency requests per
       device basis. When a device needs to be scaled to a particular frequency,
       This list is searched to find the maximum request for a given device.
       If noone has placed any request, device frequency is obtained from device
       opp table.
    2. omap_vdd_dev_list: This strcucture stores device list per vdd basis.
       Whenever a device is registered with a vdd, it is added to this list.
    3. omap_vdd_user_list: User list of devices associated with each voltage domain
       instance. The user list is implemented using plist structure with priority
       node populated with the voltage values.
    4. omap_vdd_dvfs_info: This structure is used to abstract DVFS related
       information per VDD basis. It holds pointer to corresponding vdd's
       voltagedomain instance and pointer to user list.
    
    Following helper functions have been added to operate on above data structures:
    1. omap_dvfs_add_vdd_user - function to add a user into omap_vdd_user_list
    2. omap_vdd_user_list - function to remove a user from omap_vdd_user_list
    3. omap_dvfs_register_device - function to register a device with vdd
    4. omap_dvfs_add_freq_request - function to add a frequency request into
       omap_dev_user_list
    5. omap_dvfs_remove_freq_request - function to remove a frequency request from
       omap_dev_user_list
    6. omap_dvfs_find_voltage - function to find the opp corresponding to given
       voltage
    7. omap_device_scale - this is the function that is to be used for scaling
       a specific domain. This is exported for drivers such as syslink, dspbridge
       to use it while being a module.
    
    DVFS layer is initialized and basic data structures are allocated and
    initialized as part of this.
    
    This patch is based on Vishwa and Thara's previous DVFS implementation, but with
    major rework.
    
    note: generates "warning: 'omap_dvfs_remove_vdd_user' defined but not used"
    this function will be used in future patches
    
    [nm@ti.com: misc cleanups including use voltage files from mach-omap2 dir]
    [axelhaslam@ti.com: fixed issue with ondemand - required mutex unlock]
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>
    Cc: Thara Gopinath <thara@ti.com>

---
+++ arch/arm/mach-omap2/dvfs.h
@@ -0,0 +1,35 @@
+/*
+ * OMAP3/OMAP4 DVFS Management Routines
+ *
+ * Author: Vishwanath BS	<vishwanath.bs@ti.com>
+ *
+ * Copyright (C) 2011 Texas Instruments, Inc.
+ * Vishwanath BS <vishwanath.bs@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __ARCH_ARM_MACH_OMAP2_DVFS_H
+#define __ARCH_ARM_MACH_OMAP2_DVFS_H
+#include <plat/omap_hwmod.h>
+#include "voltage.h"
+
+#ifdef CONFIG_PM
+int omap_dvfs_register_device(struct omap_hwmod *oh, struct device *dev);
+int omap_device_scale(struct device *req_dev, struct device *target_dev,
+			unsigned long rate);
+#else
+static inline int omap_dvfs_register_device(struct omap_hwmod *oh,
+		struct device *dev)
+{
+	return -EINVAL;
+}
+static inline int omap_device_scale(struct device *req_dev,
+		struct device *target_dev, unsigned long rate)
+{
+	return -EINVAL;
+}
+#endif
+#endif
